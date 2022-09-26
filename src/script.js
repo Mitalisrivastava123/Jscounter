5b9ce185183e763e2a088671dc1aaea0157dad75
Mitalisri <mitalisrivastava@cedcoss.com>
2022-09-26 14:16:12 +0530

    Javascript - Counter

--- src/script.js
+++ src/script.js
@@ -1,12 +1,14 @@
+// defining different variables 
 let add = document.getElementById("increment");
 let remove = document.getElementById("decrement");
 let number = document.getElementById("number");
 let integer = 0;
+// adding method to increment number
 add.addEventListener('click', function () {
     integer += 1;
     number.innerHTML = integer;
 });
-
+// adding method to decrement number
 remove.addEventListener('click', function () {
     integer -= 1;
     number.innerHTML = integer;
